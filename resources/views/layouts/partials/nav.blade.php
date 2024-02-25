f17163295369ce1f21a8e639ceeabd61aa4758c2
diego40g <diegopaznaula@hotmail.com>
2024-02-24 21:42:34 -0500

    adding view of navbar

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,3 +1,26 @@
-<nav>
-  <h2>This is nav</h2>
+<nav class="menu">
+  <img class="logo" src="{{asset('/img/logo.svg')}}" alt="">
+  <ul>
+    <li>
+      <a href="#">
+        <svg width="1em" height="1em" viewBox="0 0 1024 1024"><path fill="currentColor" d="M946.5 505L534.6 93.4a31.93 31.93 0 0 0-45.2 0L77.5 505c-12 12-18.8 28.3-18.8 45.3c0 35.3 28.7 64 64 64h43.4V908c0 17.7 14.3 32 32 32H448V716h112v224h265.9c17.7 0 32-14.3 32-32V614.3h43.4c17 0 33.3-6.7 45.3-18.8c24.9-25 24.9-65.5-.1-90.5"/></svg>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <svg width="1em" height="1em" viewBox="0 0 16 16"><path fill="currentColor" d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m0 6.993c1.664-1.711 5.825 1.283 0 5.132c-5.825-3.85-1.664-6.843 0-5.132"/></svg>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <span class="info-count">3</span>
+        <svg width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M6.005 9h13.938l.5-2H8.005V5h13.72a1 1 0 0 1 .97 1.243l-2.5 10a1 1 0 0 1-.97.757H5.004a1 1 0 0 1-1-1V4h-2V2h3a1 1 0 0 1 1 1zm0 14a2 2 0 1 1 0-4a2 2 0 0 1 0 4m12 0a2 2 0 1 1 0-4a2 2 0 0 1 0 4"/></svg>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <svg width="0.88em" height="1em" viewBox="0 0 448 512"><path fill="currentColor" d="M224 256a128 128 0 1 0 0-256a128 128 0 1 0 0 256m-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512h388.6c16.4 0 29.7-13.3 29.7-29.7c0-98.5-79.8-178.3-178.3-178.3z"/></svg>
+      </a>
+    </li>
+  </ul>
 </nav>
